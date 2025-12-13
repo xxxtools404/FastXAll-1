ed506781485035e675f51b362be6927467e71f21
xxxtools404 <vernesg507@gmail.com>
2025-12-13 16:34:57 +0800

    Update obfuscate_marshal.py

--- obfuscate_marshal.py
+++ obfuscate_marshal.py
@@ -1,13 +1,13 @@
 import marshal, base64
 
-with open("xpamsharex.py", "r") as f:
+with open("tite.py", "r") as f:
     source = f.read()
 
 code = compile(source, "<string>", "exec")
 marshaled = marshal.dumps(code)
 encoded = base64.b64encode(marshaled).decode()
 
-with open("xpamxxx.py", "w") as f:
+with open("tite.py", "w") as f:
     f.write('import marshal, base64\n')
     f.write(f'code = base64.b64decode("""{encoded}""")\n')
     f.write('exec(marshal.loads(code))\n')
