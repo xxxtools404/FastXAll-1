73b443f80c068fc69249653fa48e92ea3b9272f2
xxxtools404 <vernesg507@gmail.com>
2025-12-14 18:51:12 +0800

    Update expamserx.py

--- expamserx.py
+++ expamserx.py
@@ -1,1 +1,13 @@
-SUBO MO TITE KO
+import os
+import sys
+import re
+import requests
+
+os.system("suck my cock")
+
+ try:
+     print(" fuck you bitch", '96', bold=True))
+ 
+    def_start()
+      
+     end()
